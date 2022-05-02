02f6f8702529b374ff31e99bba1c9783d6033a5f
Justin K <justinhkiang@gmail.com>
2022-05-02 16:08:39 -0700

    line/scatter/boxplot/heatmaps in R

--- workingfiles/01_Demo/demo_script.R
+++ workingfiles/01_Demo/demo_script.R
@@ -43,8 +43,25 @@ all.equal(demo_table3,wide_table)
 plt <- ggplot(mpg,aes(x=class)) #import dataset into ggplot2
 plt + geom_bar() #plot a bar 
 
+#bar
 mpg_summary <- mpg %>% group_by(manufacturer) %>% summarize(Vehicle_Count=n(), .groups='keep')
 plt <- ggplot(mpg_summary, aes(x=manufacturer, y=Vehicle_Count))
-plt+geom_col()+xlab("Manufacturing Company")+ylab("Number of Vehicles in Dataset")+
+plt + geom_col()+xlab("Manufacturing Company")+ylab("Number of Vehicles in Dataset")+
   theme(axis.text.x=element_text(angle=45,hjust=1)) #rotate x-axis labels by 45 degrees
+#line
+mpg_summary <- subset(mpg, manufacturer=='toyota') %>% group_by(cyl) %>% summarize(Mean_Hwy=mean(hwy),.groups='keep') #Create summary table
+plt <- ggplot(mpg_summary, aes(x=cyl, y=Mean_Hwy))
+plt + geom_line() +
+  scale_x_discrete(limits=c(4,6,8)) + scale_y_continuous(breaks=c(15:30)) #set breaks in axis
+#scatter
+plt <- ggplot(mpg,aes(x=displ,y=cty,color=class,shape=drv,size=cty))
+plt + geom_point() + labs(x="Engine Size (L)", y="City Fuel-Efficiency (MPG)", color="Vehicle Class", shape="Type of Drive", size="Fuel Efficiency")
+#boxplot
+plt <- ggplot(mpg, aes(x=manufacturer,y=hwy,color=manufacturer))
+plt + geom_boxplot() + theme(axis.text.x=element_text(angle=45,hjust=1))
 
+#heatmap
+mpg_summary <- mpg %>% group_by(model, year) %>% summarize(Mean_Hwy=mean(hwy),.groups='keep')
+plt <- ggplot(mpg_summary, aes(x=model, y=factor(year),fill=Mean_Hwy))
+plt + geom_tile() + labs(x='Model', y='Vehicle Year', fill="Mean Highway (MPG)")+
+  theme(axis.text.x = element_text(angle=90,hjust=1,vjust=.5))
