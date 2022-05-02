76685c42aa0917a78e8fa6e79dbb4f167bcb71cb
Justin K <justinhkiang@gmail.com>
2022-05-02 16:23:52 -0700

    layers and faceting in R

--- workingfiles/01_Demo/demo_script.R
+++ workingfiles/01_Demo/demo_script.R
@@ -65,3 +65,19 @@ mpg_summary <- mpg %>% group_by(model, year) %>% summarize(Mean_Hwy=mean(hwy),.g
 plt <- ggplot(mpg_summary, aes(x=model, y=factor(year),fill=Mean_Hwy))
 plt + geom_tile() + labs(x='Model', y='Vehicle Year', fill="Mean Highway (MPG)")+
   theme(axis.text.x = element_text(angle=90,hjust=1,vjust=.5))
+
+#layers
+plt <- ggplot(mpg, aes(x=manufacturer, y=hwy))
+plt + geom_boxplot() + theme(axis.text.x=element_text(angle=45, hjust=1)) + geom_point()
+
+mpg_summary <- mpg %>% group_by(class) %>% summarize(Mean_Engine=mean(displ),SD_Engine=sd(displ), .groups='keep')
+plt <- ggplot(mpg_summary, aes(x=class, y=Mean_Engine))
+plt + geom_point(size=4) + labs(x="Vehicle Class", y="Mean Engine Size") +
+  geom_errorbar(aes(ymin=Mean_Engine-SD_Engine, ymax=Mean_Engine+SD_Engine))
+
+#faceting
+mpg_long <- mpg %>% gather(key="MPG_Type", value="Rating", c(cty,hwy))
+head(mpg_long)
+plt <- ggplot(mpg_long, aes(x=manufacturer, y=Rating, color=MPG_Type))
+plt + geom_boxplot() + facet_wrap(vars(MPG_Type)) +
+  theme(axis.text.x=element_text(angle=45, hjust=1), legend.position='none') +xlab("Manufacturer")
