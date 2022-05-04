33f979af99aad61927513bc1af6e31997a11fc19
Justin K <justinhkiang@gmail.com>
2022-05-04 13:47:26 -0700

    deliverable 2 - summary statistics on suspension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,4 @@
-library(jsonlite)
-library(tidyverse)
+library(dplyr)
 
 # Import MechaCar_mpg.csv file
 mecha_mpg <- read.csv(file='MechaCar_mpg.csv', check.names=F,stringsAsFactors=F)
@@ -11,3 +10,5 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg))
 
 # Create Visualizations for the Trip Analysis
+total_summary <- summarize(susp_coil, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+lot_summary <- summarize(susp_coil %>% group_by(Manufacturing_Lot), Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
