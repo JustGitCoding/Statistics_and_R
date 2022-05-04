afedd511287dcfbb9d3accd0a41d73ae3d0bd864
Justin K <justinhkiang@gmail.com>
2022-05-04 14:34:02 -0700

    deliverable 3 - t-tests on suspension coils by manufacturing lots

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,9 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 # Create Visualizations for the Trip Analysis
 total_summary <- summarize(susp_coil, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 lot_summary <- summarize(susp_coil %>% group_by(Manufacturing_Lot), Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+# T-tests on suspension coils
+t.test(susp_coil$PSI, mu=1500)
+t.test(subset(susp_coil, Manufacturing_Lot=='Lot1')$PSI, mu=1500)
+t.test(subset(susp_coil, Manufacturing_Lot=='Lot2')$PSI, mu=1500)
+t.test(subset(susp_coil, Manufacturing_Lot=='Lot3')$PSI, mu=1500)
